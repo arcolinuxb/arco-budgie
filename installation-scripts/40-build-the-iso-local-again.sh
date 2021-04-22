f52351f76a8a43a35775a102095346ef9996d3ff
Erik Dubois <erik.dubois@gmail.com>
2021-04-22 14:23:57 +0200

    bump version - new calamares new configs

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="budgie"
 	dmDesktop="budgie-desktop"
 
-	arcolinuxVersion='v21.05.2'
+	arcolinuxVersion='v21.05.6'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
