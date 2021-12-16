f098d1fc77746ef1074730adb147693bcb53d171
arcolinuxz <arcolinuxinfo@gmail.com>
2021-12-16 12:12:43 +0100

    bump

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="budgie"
 	dmDesktop="budgie-desktop"
 
-	arcolinuxVersion='v22.01.07'
+	arcolinuxVersion='v22.01.08'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
