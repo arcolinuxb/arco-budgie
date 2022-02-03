6e992b87f6d4d258b9134d3323ed9ebe1f626be2
arcolinuxz <arcolinuxinfo@gmail.com>
2022-02-03 08:33:14 +0100

    bump nr calamares 50

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="budgie"
 	dmDesktop="budgie-desktop"
 
-	arcolinuxVersion='v22.03.03'
+	arcolinuxVersion='v22.03.04'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
