abb8e1ed9862d567bc4d4add19d93d5a66f448c0
ArcoLinux <arcolinuxinfo@gmail.com>
2022-05-04 20:17:57 +0200

    new calamares

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="budgie"
 	dmDesktop="budgie-desktop"
 
-	arcolinuxVersion='v22.06.03'
+	arcolinuxVersion='v22.06.04'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
